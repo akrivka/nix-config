f01ad19568d3d50d4ab83549a32e3250ba242c1d
Adam Krivka <krivka.adam@gmail.com>
2025-02-13 12:02:49 +0000

    anki-sync-server

---
+++ hosts/vm-adam/services/anki-sync-server.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  port = 27701;
+in
+{
+  services.anki-sync-server = {
+    enable = true;
+    port = port;
+    baseDirectory = "/data/Adam/Services/anki";
+    users = [
+      {
+        username = "adam";
+        password = "password";
+      }
+    ];
+  };
+
+  services.caddy.virtualHosts."http://anki.adam2".extraConfig = ''
+    reverse_proxy http://[::1]:${toString port}
+  '';
+
+}
