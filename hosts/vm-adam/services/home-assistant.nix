abc416e13ed982243bb210be7ba0dd8f67c70b9d
Adam Krivka <krivka.adam@gmail.com>
2025-02-14 15:49:19 +0000

    home assistant

--- hosts/vm-adam/services/home-assistant.nix
+++ hosts/vm-adam/services/home-assistant.nix
@@ -1,17 +1,21 @@
 { config, pkgs, ... }:
 
-let 
+let
   port = 8123;
-in 
+in
 {
   services.home-assistant = {
     enable = true;
     config = {
-      http.server_port = port;
+      http = {
+        server_host = "::1";
+        trusted_proxies = [ "::1" ];
+        use_x_forwarded_for = true;
+      };
     };
   };
 
-    services.caddy.virtualHosts."http://ha.adam2".extraConfig = ''
+  services.caddy.virtualHosts."http://ha.adam2".extraConfig = ''
     reverse_proxy http://[::1]:${toString port}
   '';
 
