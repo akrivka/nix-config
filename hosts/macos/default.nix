f8b4955830d7fc01e42bf07314aa7bf6c0a51604
Adam Krivka <adam.krivka@aisle.com>
2025-07-01 23:06:29 +0200

    Important MacOS settings declared!

--- hosts/macos/default.nix
+++ hosts/macos/default.nix
@@ -48,4 +48,61 @@
   # Define the existing macOS user so that Home-Manager can infer the correct
   # home directory path (required for the `home.homeDirectory` option).
   users.users.adam.home = "/Users/adam";
+
+
+  # TO PLAY WITH LATER
+  system.defaults.NSGlobalDomain = {
+    # ACTUALLY IMPORTANT STUFF
+    "com.apple.swipescrolldirection" = false;
+    "com.apple.trackpad.scaling" = 1.5;
+    InitialKeyRepeat = 15;
+    KeyRepeat = 2;
+
+    AppleICUForce24HourTime = true;
+    AppleInterfaceStyleSwitchesAutomatically = true;
+    AppleMeasurementUnits = "Centimeters";
+    AppleMetricUnits = 1;
+    AppleShowScrollBars = "Automatic";
+    AppleTemperatureUnit = "Celsius";
+    NSAutomaticCapitalizationEnabled = false;
+    NSAutomaticDashSubstitutionEnabled = false;
+    NSAutomaticPeriodSubstitutionEnabled = false;
+    NSAutomaticQuoteSubstitutionEnabled = true;
+    _HIHideMenuBar = false;
+  };
+
+  # Dock and Mission Control
+  system.defaults.dock = {
+    autohide = true;
+    expose-group-apps = false;
+    mru-spaces = false;
+    tilesize = 128;
+    # Disable all hot corners
+    wvous-bl-corner = 1;
+    wvous-br-corner = 1;
+    wvous-tl-corner = 1;
+    wvous-tr-corner = 1;
+  };
+
+  # Login and lock screen
+  system.defaults.loginwindow = {
+    GuestEnabled = false;
+    DisableConsoleAccess = true;
+  };
+
+  # Spaces
+  system.defaults.spaces.spans-displays = false;
+
+  # Trackpad
+  system.defaults.trackpad = {
+    Clicking = false;
+    TrackpadRightClick = true;
+  };
+
+  # Finder
+  system.defaults.finder = {
+    FXEnableExtensionChangeWarning = true;
+    NewWindowTarget = "Home";
+    ShowPathbar = true;
+  };
 }
