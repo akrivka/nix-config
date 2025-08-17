63627d4ea97541baf753795f0e2962d79173f49a
Adam Krivka <krivka.adam@gmail.com>
2025-08-17 19:16:00 +0200

    add password

--- vms/vm-public/services/caddy.nix
+++ vms/vm-public/services/caddy.nix
@@ -16,10 +16,15 @@
     virtualHosts."http://cmg.akrivka.com" = {
       extraConfig = ''
         root * /var/www/cmg.akrivka.com
+        basicauth * {
+          cmg {env.CMG_AUTH_HASH}
+        }
         file_server
       '';
     };
   };
+
+  systemd.services.caddy.environment.CMG_AUTH_HASH = "$2a$14$hbn7.kEAh82p0Bk9K2/1s.skOe.O8O/MwMDNF6zMGA2LIDAlQ5wRa";
 
   networking.firewall.allowedTCPPorts = [
     80
