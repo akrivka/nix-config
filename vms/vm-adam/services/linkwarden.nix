780a736cb7878a721b60175ecd333b11442a78bd
Adam Krivka <krivka.adam@gmail.com>
2025-10-11 15:48:54 +0200

    Linkwarden

---
+++ vms/vm-adam/services/linkwarden.nix
@@ -0,0 +1,20 @@
+{
+  ...
+}:
+
+let
+  # Linkwarden app port on the host (loopback only)
+  port = 3110;
+in
+{
+  services.linkwarden = {
+    enable = true;
+    port = port;
+    storageLocation = "/data/Adam/Services/linkwarden";
+  };
+
+  # Caddy reverse proxy for local domain
+  services.caddy.virtualHosts."http://linkwarden.adam".extraConfig = ''
+    reverse_proxy localhost:${toString port}
+  '';
+}
